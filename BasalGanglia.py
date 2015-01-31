neuron bias gain implemented sign of d1 d2 pathway fixed, low firing rate softmax tested success ratio almost good for long simulations

BasalGanglia.py
@@ -190,8 +190,9 @@ class BasalGanglia(object):
                 if other != nactions:
                     #nest.DivergentConnect(self.strD1[nactions], self.strD1[other], model=self.params['lateral_synapse_d1']  )
                     #nest.DivergentConnect(self.strD2[nactions], self.strD2[other], model=self.params['lateral_synapse_d2']  )
-                    nest.RandomDivergentConnect(self.strD1[nactions], self.strD1[other], int(self.params['num_msn_d1']/self.params['ratio_lat_inh_d1_d1']) , weight = self.params['inhib_lateral_weights_d1'], delay= self.params['inhib_lateral_delay_d1'])
-                    nest.RandomDivergentConnect(self.strD2[nactions], self.strD2[other], int(self.params['num_msn_d2']/self.params['ratio_lat_inh_d2_d2']), weight = self.params['inhib_lateral_weights_d2'], delay= self.params['inhib_lateral_delay_d2'])
+                    nest.RandomDivergentConnect(self.strD1[nactions], self.strD1[other], int(self.params['num_msn_d1']/self.params['ratio_lat_inh_d1_d1']) , weight = np.round(np.random.normal(self.params['inhib_lateral_weights_d1'], self.params['std_inhib_lateral_weights_d1'], int(self.params['num_msn_d1']/self.params['ratio_lat_inh_d1_d1'])),1).tolist(), delay= np.round(np.random.normal(self.params['inhib_lateral_delay_d1'],self.params['std_inhib_lateral_delay_d1'], int(self.params['num_msn_d1']/self.params['ratio_lat_inh_d1_d1'])),1).tolist() )
+                    nest.RandomDivergentConnect(self.strD2[nactions], self.strD2[other], int(self.params['num_msn_d2']/self.params['ratio_lat_inh_d2_d2']) , weight = np.round(np.random.normal(self.params['inhib_lateral_weights_d2'], self.params['std_inhib_lateral_weights_d2'], int(self.params['num_msn_d2']/self.params['ratio_lat_inh_d2_d2'])),1).tolist(), delay= np.round(np.random.normal(self.params['inhib_lateral_delay_d2'],self.params['std_inhib_lateral_delay_d2'], int(self.params['num_msn_d2']/self.params['ratio_lat_inh_d2_d2'])),1).tolist() )
+                    #nest.RandomDivergentConnect(self.strD2[nactions], self.strD2[other], int(self.params['num_msn_d2']/self.params['ratio_lat_inh_d2_d2']), weight = self.params['inhib_lateral_weights_d2'], delay= self.params['inhib_lateral_delay_d2'])
                 
                 #nest.RandomDivergentConnect(self.strD2[nactions], self.strD1[nactions], int(self.params['num_msn_d1']/4.), weight = self.params['inhib_lateral_weights_d2_d1'], delay= self.params['inhib_lateral_delay_d2'])
         
@@ -232,17 +233,17 @@ class BasalGanglia(object):
             for iaction in range(self.params['n_actions']):
                # nest.SetDefaults( self.params['dopa_bcpnn'], params= self.params['params_dopa_bcpnn_actions_rp'])
                 #nest.DivergentConnect(self.strD1[iaction], self.rp[iaction+istate*self.params['n_actions']], weight=self.params['weight_actions_rp'], delay=self.params['delay_actions_rp'])
-                nest.DivergentConnect(self.efference_copy[iaction], self.rp[iaction+istate*self.params['n_actions']], weight=self.params['weight_efference_rp'], delay=self.params['delay_efference_rp'])
+                nest.DivergentConnect(self.efference_copy[iaction], self.rp[iaction+istate*self.params['n_actions']], weight=np.round(np.random.normal(self.params['weight_efference_rp'], self.params['std_weight_efference_rp'], self.params['num_rp_neurons']), 1).tolist(), delay=np.round(np.random.normal(self.params['delay_efference_rp'], self.params['std_delay_efference_rp'], self.params['num_rp_neurons']), 1).tolist())
                # print 'DATADATA',  nest.GetStatus(nest.GetConnections(self.actions[iaction], self.rp[iaction+istate*self.params['n_actions']]))        
                # nest.SetDefaults( self.params['dopa_bcpnn'], params= self.params['params_dopa_bcpnn_states_rp'])
-                nest.DivergentConnect(self.states[istate], self.rp[iaction + istate*self.params['n_actions']], weight=self.params['weight_states_rp'], delay=self.params['delay_states_rp'] )
+                nest.DivergentConnect(self.states[istate], self.rp[iaction + istate*self.params['n_actions']], weight=np.round(np.random.normal(self.params['weight_states_rp'], self.params['std_weight_states_rp'],self.params['num_rp_neurons']), 1).tolist(), delay=np.round(np.random.normal(self.params['delay_states_rp'],self.params['std_delay_states_rp'],self.params['num_rp_neurons']), 1 ).tolist())
         
         # ################### #
         # STRIATUM / ACTIONS  #
         # ################### #
         for nactions in xrange(self.params['n_actions']):
-            nest.ConvergentConnect(self.strD1[nactions], self.actions[nactions], weight=self.params['str_to_output_inh_w'], delay=self.params['str_to_output_inh_delay']) 
-            nest.ConvergentConnect(self.strD2[nactions], self.actions[nactions], weight=self.params['str_to_output_exc_w'], delay=self.params['str_to_output_exc_delay'])	
+            nest.ConvergentConnect(self.strD1[nactions], self.actions[nactions], weight=np.round(np.random.normal(self.params['str_to_output_inh_w'],self.params['std_str_to_output_inh_w'],self.params['num_msn_d1']),1).tolist(), delay=np.round(np.random.normal(self.params['str_to_output_inh_delay'],self.params['std_str_to_output_inh_delay'], self.params['num_msn_d1']),1).tolist()) 
+            nest.ConvergentConnect(self.strD2[nactions], self.actions[nactions], weight=np.round(np.random.normal(self.params['str_to_output_exc_w'],self.params['std_str_to_output_inh_w'],self.params['num_msn_d2']),1).tolist(), delay=np.round(np.random.normal(self.params['str_to_output_exc_delay'],self.params['std_str_to_output_exc_delay'], self.params['num_msn_d2']),1).tolist())	
             # for neuron in self.actions[nactions]:
             #     nest.ConvergentConnect(self.strD1[nactions], [neuron], weight=self.params['str_to_output_inh_w'], delay=self.params['str_to_output_inh_delay']) 
             #     nest.ConvergentConnect(self.strD2[nactions], [neuron], weight=self.params['str_to_output_exc_w'], delay=self.params['str_to_output_exc_delay'])	
@@ -252,10 +253,11 @@ class BasalGanglia(object):
         for nactions in xrange(self.params['n_actions']):
             for i in xrange(self.params['n_actions']):
                  if i != nactions:
-                     nest.DivergentConnect(self.efference_copy[nactions], self.strD1[i], weight=self.params['weight_efference_strd1_inh'], delay=self.params['delay_efference_strd1_inh'])
-                     nest.DivergentConnect(self.efference_copy[nactions], self.strD2[i], weight=self.params['weight_efference_strd2_inh'], delay=self.params['delay_efference_strd2_inh'])
-            nest.DivergentConnect(self.efference_copy[nactions], self.strD1[nactions], weight=self.params['weight_efference_strd1_exc'], delay=self.params['delay_efference_strd1_exc'])
-            nest.DivergentConnect(self.efference_copy[nactions], self.strD2[nactions], weight=self.params['weight_efference_strd2_exc'], delay=self.params['delay_efference_strd2_exc'])
+                     nest.DivergentConnect(self.efference_copy[nactions], self.strD1[i], weight=np.round(np.random.normal(self.params['weight_efference_strd1_inh'],self.params['std_weight_efference_strd1_inh'], self.params['num_msn_d1']),1).tolist(), delay=np.round(np.random.normal(self.params['delay_efference_strd1_inh'],self.params['std_delay_efference_strd1_inh'], self.params['num_msn_d1']),1).tolist() )
+                     nest.DivergentConnect(self.efference_copy[nactions], self.strD2[i], weight=np.round(np.random.normal(self.params['weight_efference_strd2_inh'],self.params['std_weight_efference_strd2_inh'], self.params['num_msn_d2']),1).tolist(), delay=np.round(np.random.normal(self.params['delay_efference_strd2_inh'],self.params['std_delay_efference_strd2_inh'], self.params['num_msn_d2']),1).tolist() )
+                     #nest.DivergentConnect(self.efference_copy[nactions], self.strD2[i], weight=self.params['weight_efference_strd2_inh'], delay=self.params['delay_efference_strd2_inh'])
+            nest.DivergentConnect(self.efference_copy[nactions], self.strD1[nactions], weight=np.round(np.random.normal(self.params['weight_efference_strd1_exc'],self.params['std_weight_efference_strd1_exc'], self.params['num_msn_d1']),1).tolist(), delay=np.round(np.random.normal(self.params['delay_efference_strd1_exc'], self.params['std_delay_efference_strd1_exc'], self.params['num_msn_d1']),1).tolist())
+            nest.DivergentConnect(self.efference_copy[nactions], self.strD2[nactions], weight=np.round(np.random.normal(self.params['weight_efference_strd2_exc'],self.params['std_weight_efference_strd2_exc'], self.params['num_msn_d2']),1).tolist(), delay=np.round(np.random.normal(self.params['delay_efference_strd2_exc'], self.params['std_delay_efference_strd2_exc'], self.params['num_msn_d2']),1).tolist())
 
 
 
@@ -280,6 +282,16 @@ class BasalGanglia(object):
 
         for index_rp in xrange(self.params['n_actions'] * self.params['n_states']):
             nest.DivergentConnect( self.rp[index_rp], self.rew, model = self.params['synapse_RP']  )
+            conn = nest.GetConnections(source=self.rp[index_rp], target=self.rew, synapse_model=self.params['synapse_RP'])
+            delay_params = [{'delay':np.round(np.random.normal(self.params['delay_rp'], self.params['std_delay_rp']),1)} for c in conn]
+            nest.SetStatus(conn, delay_params)
+            pi_params = [{'p_i':max(self.params['positive_prior'], np.round(np.random.normal(self.params['p_i_rp'], self.params['std_p_i_rp']),1))} for c in conn]
+            pj_params = [{'p_j':max(self.params['positive_prior'], np.round(np.random.normal(self.params['p_j_rp'], self.params['std_p_j_rp']),1))} for c in conn]
+            #pij_params = [{'p_ij':max(self.params['positive_prior']*self.params['positive_prior']+self.params['positive_prior'], np.round(np.random.normal(self.params['p_ij_rp'], self.params['std_p_ij_rp']),1))} for c in conn]
+            pij_params = [{'p_ij':max(self.params['epsilon'], np.round(np.random.normal(self.params['p_ij_rp'], self.params['std_p_ij_rp']),1))} for c in conn]
+            nest.SetStatus(conn, pi_params)
+            nest.SetStatus(conn, pj_params)
+            nest.SetStatus(conn, pij_params)
        # nodes_info = nest.GetStatus( nest.GetConnections(self.rp, self.rew))
        # local_nodes = [(ni['global_id'], ni['vp']) for ni in nodes_info if ni['local']]
        # for gid, vp in local_nodes:
@@ -296,19 +308,33 @@ class BasalGanglia(object):
         print 'Geronimod2 ', self.comm.rank, 'params',nest.GetDefaults(self.params['synapse_d2'])
         for nstates in range(self.params['n_states']):
             for nactions in range(self.params['n_actions']):
+                # D1
                 #nest.SetDefaults(self.params['dopa_bcpnn'], params=self.params['params_dopa_bcpnn_d1'])
                 nest.DivergentConnect(self.states[nstates], self.strD1[nactions], model=self.params['synapse_d1'])
+                conn = nest.GetConnections(source=self.states[nstates], target=self.strD1[nactions], synapse_model=self.params['synapse_d1'])
+                delay_params = [{'delay':np.round(np.random.normal(self.params['delay_d1'], self.params['std_delay_d1']),1)} for c in conn]
+                nest.SetStatus(conn, delay_params)
+                pi_params = [{'p_i':max(self.params['epsilon'], np.round(np.random.normal(self.params['p_i_d1'], self.params['std_p_i_d1']),1))} for c in conn]
+                pj_params = [{'p_j':max(self.params['epsilon'], np.round(np.random.normal(self.params['p_j_d1'], self.params['std_p_j_d1']),1))} for c in conn]
+                pij_params = [{'p_ij':max(self.params['positive_prior']*self.params['positive_prior']+self.params['positive_prior'], np.round(np.random.normal(self.params['p_ij_d1'], self.params['std_p_ij_d1']),1))} for c in conn]
+                nest.SetStatus(conn, pi_params)
+                nest.SetStatus(conn, pj_params)
+                nest.SetStatus(conn, pij_params)
+                # D2
                 #nest.SetDefaults(self.params['dopa_bcpnn'], params=self.params['params_dopa_bcpnn_d2'])
                 nest.DivergentConnect(self.states[nstates], self.strD2[nactions], model=self.params['synapse_d2'])
-                # Normal distribution of P traces
-               # nest.SetStatus(nest.GetConnections(self.states[nstates], self.strD1[nactions]), {'p_i': pyrngs[vp].normal(self.params['p_i'], self.params['p_i_std']), 'p_j': pyrngs[vp].normal(self.params['p_j'], self.params['p_j_std']), 'p_ij': pyrngs[vp].normal(self.params['p_ij'], self.params['p_ij_std'])})
-                #nodes_info = nest.GetStatus(nest.GetConnections(self.states[nstates], self.strD2[nactions]))
-                #local_nodes = [(ni['global_id'], ni['vp']) for ni in nodes_info if ni['local']]
-                #for gid, vp in local_nodes:
-               # nest.SetStatus(nest.GetConnections(self.states[nstates], self.strD2[nactions]), {'p_i': pyrngs[vp].normal(self.params['p_i'], self.params['p_i_std']), 'p_j': pyrngs[vp].normal(self.params['p_j'], self.params['p_j_std']), 'p_ij': pyrngs[vp].normal(self.params['p_ij'], self.params['p_ij_std'])})
+                conn = nest.GetConnections(source=self.states[nstates], target=self.strD2[nactions], synapse_model=self.params['synapse_d2'])
+                delay_params = [{'delay':np.round(np.random.normal(self.params['delay_d2'], self.params['std_delay_d2']),1)} for c in conn]
+                nest.SetStatus(conn, delay_params)
+                pi_params = [{'p_i':max(self.params['epsilon'],np.round(np.random.normal(self.params['p_i_d2'], self.params['std_p_i_d2']),1))} for c in conn]
+                pj_params = [{'p_j':max(self.params['epsilon'],np.round(np.random.normal(self.params['p_j_d2'], self.params['std_p_j_d2']),1))} for c in conn]
+                pij_params = [{'p_ij':max(self.params['positive_prior']*self.params['positive_prior']+self.params['positive_prior'], np.round(np.random.normal(self.params['p_ij_d2'], self.params['std_p_ij_d2']),1))} for c in conn]
+                nest.SetStatus(conn, pi_params)
+                nest.SetStatus(conn, pj_params)
+                nest.SetStatus(conn, pij_params)
             
-        connectiond1 = nest.GetConnections(target = [self.strD1[0][0]])
-        connectiond2 = nest.GetConnections(target = [self.strD2[0][0]])
+        #connectiond1 = nest.GetConnections(target = [self.strD1[0][0]])
+        #connectiond2 = nest.GetConnections(target = [self.strD2[0][0]])
         self.comm.barrier()
         if self.pc_id ==1:
             print 'GETCONNECT neuron 1'
@@ -422,18 +448,18 @@ class BasalGanglia(object):
             nest.SetStatus(self.efference_copy[nactions], {'rate' : self.params['inactive_efference_rate']})
         #    print 'debug: EFFERENCE OFF for ACTION', nactions  , 'activity is: ',nest.GetStatus(self.efference_copy[nactions])[0]['rate'] 
         nest.SetStatus(self.efference_copy[action], {'rate' : self.params['active_full_efference_rate']})
-       # print 'debug: EFFERENCE SET for ACTION',action ,' activity is: ',nest.GetStatus(self.efference_copy[action])[0]['rate'] 
+        print 'debug: EFFERENCE SET for ACTION',action ,' activity is: ',nest.GetStatus(self.efference_copy[action])[0]['rate'] 
         
     def stop_efference(self):
         for nactions in xrange(self.params['n_actions']):
             nest.SetStatus(self.efference_copy[nactions], {'rate' : self.params['inactive_efference_rate']})
-            print 'debug: EFFERENCE OFF, activity is: ',nest.GetStatus(self.efference_copy[nactions])[0]['rate'] 
+            #print 'debug: EFFERENCE OFF, activity is: ',nest.GetStatus(self.efference_copy[nactions])[0]['rate'] 
     
     
     
     def set_rp(self, state, action, gain, kappa):
         if self.params['params_dopa_bcpnn_RP']['dopamine_modulated']:
-            nest.SetStatus(nest.GetConnections( self.rp[ action + state*self.params['n_actions'] ], self.rew ),{'gain':gain})
+            nest.SetStatus(nest.GetConnections( self.rp[ action + state*self.params['n_actions'] ], self.rew ),{'gain':gain*self.params['gain_rp']})
         else:
             nest.SetStatus(nest.GetConnections( self.rp[ action + state*self.params['n_actions'] ], self.rew ),{'gain':gain, 'K':kappa})
 
@@ -593,10 +619,10 @@ class BasalGanglia(object):
        for nstate in range(self.params['n_states']):
            for naction in range(self.params['n_actions']):
                # pp.pprint(nest.GetStatus(nest.GetConnections(self.states[nstate], self.strD1[naction], self.params['synapse_d1'])))
-               nest.SetStatus(self.strD1[naction], {'gain':gain*self.params['gain_neuron']})
-               nest.SetStatus(self.strD2[naction], {'gain':gain*self.params['gain_neuron']})
-               nest.SetStatus(nest.GetConnections(self.states[nstate], self.strD1[naction], self.params['synapse_d1']), {'gain':gain})
-               nest.SetStatus(nest.GetConnections(self.states[nstate], self.strD2[naction]), {'gain':gain})
+              # nest.SetStatus(self.strD1[naction], {'gain':gain*self.params['gain_neuron']})
+              # nest.SetStatus(self.strD2[naction], {'gain':gain*self.params['gain_neuron']})
+               nest.SetStatus(nest.GetConnections(self.states[nstate], self.strD1[naction], self.params['synapse_d1']), {'gain':gain*self.params['gain_d1']})
+               nest.SetStatus(nest.GetConnections(self.states[nstate], self.strD2[naction]), {'gain':gain*self.params['gain_d2']})
     	
      #  for index_rp in range(self.params['n_actions'] * self.params['n_states']):
      #       for naction in range(self.params['n_actions']):

main.py
@@ -113,17 +113,20 @@ if __name__ == '__main__':
     #weights
     staten2D1 = np.empty(( params['n_recordings'], params['n_actions'] ))
     staten2D2 = np.empty(( params['n_recordings'], params['n_actions']))
+    stateb2D1 = np.empty(( params['n_recordings'], params['n_actions'] ))
+    stateb2D2 = np.empty(( params['n_recordings'], params['n_actions']))
     state2D1s = np.empty(( params['n_recordings'], params['n_states'] ))
     state2D2s = np.empty(( params['n_recordings'], params['n_states'] ))
     br_w0 = np.empty(( params['n_recordings'], params['n_actions']))
     br_w1 = np.empty(( params['n_recordings'], params['n_actions']))
     br_w2 = np.empty(( params['n_recordings'], params['n_actions']))
     rp_w = np.zeros(( params['n_recordings'], params['n_states'] * params['n_actions']))
+    rp_bias = np.zeros(( params['n_recordings'], params['n_states'] * params['n_actions']))
     
-    list_d1 = [ staten2D1, [],[],[],[],[],[],[], state2D1s ]
-    list_d2 = [ staten2D2, [],[],[],[],[],[],[], state2D2s ]
+    list_d1 = [ staten2D1, [],[],[],[],[],[],[], stateb2D1 ]
+    list_d2 = [ staten2D2, [],[],[],[],[],[],[], stateb2D2 ]
     list_br = [ [],[],[],[], br_w0, br_w1, br_w2 ]
-    list_rp = [ rp_w , [],[],[]]
+    list_rp = [ rp_w , [],[], rp_bias]
     
     
     if not params['light_record']:
@@ -207,7 +210,8 @@ if __name__ == '__main__':
     else:
         dopa_kf, dopa_k, dopa_m, dopa_n, list_d1, list_d2, list_br, list_rp = mynest.Simulate(params['t_init'], params['resolution'], params['n_actions'], params['n_states'], BG, dopa_kf, dopa_k, dopa_m, dopa_n, list_d1, list_d2, list_br, list_rp,comm)
 
-    BG.set_gain(params['gain'])
+    #BG.set_gain(params['gain'])
+    BG.set_gain(1.)
     BG.set_noise()
     BG.stop_efference()
 #####   ###################         ###############
@@ -286,7 +290,7 @@ if __name__ == '__main__':
             # BG.set_kappa_ON(-params['rpe'], states[iteration], actions[iteration])
             BG.no_reward()
         #BG.set_reward(rew)
-        BG.set_rp(states[iteration], actions[iteration], params['gain_rp'] , 1.)
+        BG.set_rp(states[iteration], actions[iteration], 1. , 1.)
      #   BG.stop_efference()
 
         if comm != None:
@@ -299,7 +303,8 @@ if __name__ == '__main__':
             comm.barrier()
 
         BG.set_rp(states[iteration], actions[iteration], 0., 0. )
-        BG.set_gain(params['gain'])
+        #BG.set_gain(params['gain'])
+        BG.set_gain(1.)
       #  BG.set_kappa_OFF()
         BG.set_rest()
         if comm != None:
@@ -513,6 +518,30 @@ if __name__ == '__main__':
             print 'Im process ', pc_id, 'VTmod is ', nest.GetStatus(BG.vt_dopa) 
 
         # print 'weight simu ', weights_sim
+    if pc_id ==3:	
+        #print 'bias_d1', list_d1[8]
+        #print 'bias_d2', list_d2[8]
+        #print 'bias_rp', list_rp[3]
+        pl.figure(301)
+        pl.subplot(211)
+        pl.title('D1')
+        pl.plot(list_d1[8])
+        pl.ylabel(r'$b_{j}$')
+        pl.subplot(212)
+        pl.title('D2')
+        pl.plot(list_d2[8])
+        pl.ylabel(r'$b_{j}$')
+        pl.xlabel('trials')
+        pl.suptitle('Computed biases from state '+str(params['recorded'])+' to all actions')
+        pl.savefig('fig301_bias_allactions.pdf')
+
+        pl.figure(302)
+        pl.title('RP bias')
+        pl.plot(list_rp[3])
+        pl.ylabel(r'$b_{j}$')
+        pl.xlabel('trials')
+        pl.savefig('fig302_bias_rp.pdf')
+
     if pc_id ==1:	
         t1 = time.time() - t0
         print 'Time: %.2f [sec] %.2f [min]' % (t1, t1 / 60.)

mynest_light.py
@@ -37,11 +37,13 @@ def Simulate(time, resolution, nactions, nstates, BG, dopakf, dopak, dopam, dopa
         dkf[i], dk[i], dm[i], dn[i] =  nest.GetStatus(BG.conn_dopa2, ['k_filtered', 'k', 'm', 'n'])[0]
         for q in xrange(nactions):
             #conn = nest.GetConnections(source = BG.states[BG.who], target = BG.strD1[q], synapse_model = 'bcpnn_dopamine_synapse_d1')
-            list_d1[0][BG.rec_count,q] = np.mean([(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(BG.conn_d1[q])])
+            #list_d1[0][BG.rec_count,q] = np.mean([(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(BG.conn_d1[q])])
+            #list_d1[8][BG.rec_count,q] = np.mean([np.log(a['p_j']) for a in nest.GetStatus(BG.conn_d1[q])])
             #sd1w[q] = np.mean([(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(conn)])
             #conn = nest.GetConnections(source = BG.states[BG.who], target = BG.strD2[q], synapse_model = 'bcpnn_dopamine_synapse_d2')
             #sd2w[q] = np.mean([(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(conn)])
-            list_d2[0][BG.rec_count,q] = np.mean([(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(BG.conn_d2[q])])
+            #list_d2[0][BG.rec_count,q] = np.mean([(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(BG.conn_d2[q])])
+            #list_d2[8][BG.rec_count,q] = np.mean([np.log(a['p_j']) for a in nest.GetStatus(BG.conn_d2[q])])
             #conn = nest.GetConnections(source = BG.states[BG.who], target = BG.brainstem[q], synapse_model = 'bcpnn_synapse')
             list_br[4][BG.rec_count,q] = np.mean([(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(BG.conn_habit0[q])])
             #conn = nest.GetConnections(source = BG.states[1], target = BG.brainstem[q], synapse_model = 'bcpnn_synapse')
@@ -52,11 +54,13 @@ def Simulate(time, resolution, nactions, nstates, BG, dopakf, dopak, dopam, dopa
             if rank == 0:
                 #conn_d1 = (nest.GetConnections( source = BG.states[BG.who], target=BG.strD1[q], synapse_model='bcpnn_dopamine_synapse_d1' ))
                 log = [np.log(a['p_ij']/(a['p_i']*a['p_j'])) for a in nest.GetStatus(BG.conn_d1[q]) ]
-                print 'lolog', log
+                #print 'lolog1', log, 'len = ', len(log), ' avg = ', np.mean(log)
                 for i_proc in xrange(1,size):
                     log = np.r_[log, comm.recv(source=i_proc)]
             else:
-                comm.send( [np.log(a['p_ij']/ (a['p_j']* a['p_i'])) for a in nest.GetStatus(BG.conn_d1[q])], dest=0)
+                data = [np.log(a['p_ij']/ (a['p_j']* a['p_i'])) for a in nest.GetStatus(BG.conn_d1[q])]
+                comm.send( data , dest=0)
+                #print 'logelse id = ', rank, 'mean log = ', np.mean(data)
 
             if rank == 0:
                 avg_d1 = np.mean(log)
@@ -66,7 +70,7 @@ def Simulate(time, resolution, nactions, nstates, BG, dopakf, dopak, dopam, dopa
             if rank == 0:
                 #conn_d2 = (nest.GetConnections( source = BG.states[BG.who], target=BG.strD2[q], synapse_model='bcpnn_dopamine_synapse_d2' ))
                 log = [np.log(a['p_ij']/(a['p_i']*a['p_j'])) for a in nest.GetStatus(BG.conn_d2[q]) ]
-                print 'lolog', log
+                #print 'lolog2', log, 'len = ', len(log), ' avg = ', np.mean(log)
                 for i_proc in xrange(1,size):
                     log = np.r_[log, comm.recv(source=i_proc)]
             else:
@@ -78,6 +82,38 @@ def Simulate(time, resolution, nactions, nstates, BG, dopakf, dopak, dopam, dopa
             list_d2[0][BG.rec_count,q] = comm.bcast(list_d2[0][BG.rec_count,q], root=0)
             comm.barrier()
 
+            if rank == 0:
+                bias_d1 = [np.log(a['p_j']) for a in nest.GetStatus(BG.conn_d1[q]) ]
+                #print 'bias_d1', bias_d1, 'len = ', len(bias_d1), ' avg = ', np.mean(bias_d1)
+                for i_proc in xrange(1,size):
+                    bias_d1 = np.r_[bias_d1, comm.recv(source=i_proc)]
+            else:
+                bias_d1 = [np.log(a['p_j']) for a in nest.GetStatus(BG.conn_d1[q]) ]
+                #print 'ELSE_bias_d1', bias_d1, 'len = ', len(bias_d1), ' avg = ', np.mean(bias_d1)
+                comm.send( bias_d1 , dest=0)
+                #print 'logelse id = ', rank, 'mean log = ', np.mean(bias_d1)
+
+            if rank == 0:
+                avg_bias_d1 = np.mean(bias_d1)
+                list_d1[8][BG.rec_count, q] = avg_bias_d1
+            list_d1[8][BG.rec_count,q] = comm.bcast(list_d1[8][BG.rec_count,q], root=0)
+            comm.barrier()
+
+            if rank == 0:
+                bias_d2 = [np.log(a['p_j']) for a in nest.GetStatus(BG.conn_d2[q]) ]
+                #print 'bias_d2', bias_d2, 'len = ', len(bias_d2), ' avg = ', np.mean(bias_d2)
+                for i_proc in xrange(1,size):
+                    bias_d2 = np.r_[bias_d2, comm.recv(source=i_proc)]
+            else:
+                bias_d2 = [np.log(a['p_j']) for a in nest.GetStatus(BG.conn_d2[q]) ]
+                #print 'ELSE_bias_d2','else id = ', rank, bias_d2, 'len = ', len(bias_d2), ' avg = ', np.mean(bias_d2)
+                comm.send( bias_d2 , dest=0)
+
+            if rank == 0:
+                avg_bias_d2 = np.mean(bias_d2)
+                list_d2[8][BG.rec_count, q] = avg_bias_d2
+            list_d2[8][BG.rec_count,q] = comm.bcast(list_d2[8][BG.rec_count,q], root=0)
+            comm.barrier()
 
             if rank == 0:
                 print q , ' MUTAVG = ', avg_d1, 'SINGLE = ', list_d1[0][BG.rec_count,q]
@@ -90,7 +126,41 @@ def Simulate(time, resolution, nactions, nstates, BG, dopakf, dopak, dopam, dopa
 #            sd2sw[p] = [(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(conn)]
         for n in xrange(nactions*nstates):
             #connrp = nest.GetConnections(source= BG.rp[n], target = BG.rew, synapse_model='bcpnn_dopa_synapse_RP')
-            list_rp[0][BG.rec_count,n] = np.mean([(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(BG.conn_rp[n])])
+            #list_rp[0][BG.rec_count,n] = np.mean([(np.log(a['p_ij']/(a['p_i']*a['p_j']))) for a in nest.GetStatus(BG.conn_rp[n])])
+            #list_rp[3][BG.rec_count,n] = np.mean([np.log(a['p_j']) for a in nest.GetStatus(BG.conn_rp[n])])
+            if rank == 0:
+                rp_bias = [np.log(a['p_j']) for a in nest.GetStatus(BG.conn_rp[n]) ]
+                print 'bias_rp', rp_bias, 'len = ', len(rp_bias), ' avg = ', np.mean(rp_bias)
+                for i_proc in xrange(1,size):
+                    rp_bias= np.r_[rp_bias, comm.recv(source=i_proc)]
+            else:
+                rp_bias = [np.log(a['p_j']) for a in nest.GetStatus(BG.conn_rp[n]) ]
+                print 'bias_rp id', rank, 'bias = ',  rp_bias, 'len = ', len(rp_bias), ' avg = ', np.mean(rp_bias)
+                comm.send( rp_bias , dest=0)
+
+            if rank == 0:
+                avg_rp_bias = np.mean(rp_bias)
+                list_rp[3][BG.rec_count, n] = avg_rp_bias
+            list_rp[3][BG.rec_count,n] = comm.bcast(list_rp[3][BG.rec_count,n], root=0)
+            comm.barrier()
+
+
+
+            if rank == 0:
+                rp_w = [np.log(a['p_ij']/(a['p_i']*a['p_j'])) for a in nest.GetStatus(BG.conn_rp[n]) ]
+                #print 'w_rp', rp_w, 'len = ', len(rp_w), ' avg = ', np.mean(rp_w)
+                for i_proc in xrange(1,size):
+                    rp_w= np.r_[rp_w, comm.recv(source=i_proc)]
+            else:
+                rp_w = [np.log(a['p_ij']/(a['p_i']*a['p_j'])) for a in nest.GetStatus(BG.conn_rp[n]) ]
+                #print 'w_rp id', rank, 'bias = ',  rp_w, 'len = ', len(rp_w), ' avg = ', np.mean(rp_w)
+                comm.send( rp_w , dest=0)
+
+            if rank == 0:
+                avg_rp_w = np.mean(rp_w)
+                list_rp[0][BG.rec_count, n] = avg_rp_w
+            list_rp[0][BG.rec_count,n] = comm.bcast(list_rp[0][BG.rec_count,n], root=0)
+            comm.barrier()
  
 
 #        list_d1[0][BG.rec_count,:] =  sd1w

simulation_parameters.py
@@ -69,14 +69,14 @@ class global_parameters(ParameterContainer.ParameterContainer):
         # time % (modulo) resolution has to be 0  --> for Figures processing
         self.params['resolution'] = 250.   # [ms]
         # after this time the input stimulus will be transformed
-        self.params['block_len'] = 2       # number of trials in a block
-        self.params['n_blocks'] = 3        # number of blocks
+        self.params['block_len'] = 3       # number of trials in a block
+        self.params['n_blocks'] = 5        # number of blocks
         self.params['dt'] = .1             # [ms] /default .1
 
         self.params['record_spikes'] = True
         self.params['record_voltages'] = False 
         self.params['light_record'] = True
-        self.params['softmax'] = False
+        self.params['softmax'] = True
 
         self.params['threshold']= 0.05
 
@@ -99,23 +99,24 @@ class global_parameters(ParameterContainer.ParameterContainer):
         self.params['tau_j'] = 6.
         self.params['tau_e'] = 40.
         self.params['tau_p'] = 1000. #1000.
-        self.params['gain'] = 4. #2.5 ##1.9   #3.
-        self.params['gain_d1'] = 4. # 1. ##1.9   #3.
-        self.params['gain_d2'] = 4. # 1. ##1.9   #3.
+        self.params['gain'] = 6. #3.5 #2.5 ##1.9   #3.
+        self.params['gain_d1'] = 6. #3.5 # 1. ##1.9   #3.
+        self.params['gain_d2'] = 6.  #3.5 # 1. ##1.9   #3.
         self.params['gain_rp'] = -3.
         self.params['gain_dopa'] = 6.  #5.   #4.
         self.params['gain_neuron'] = 20.       #gain for the neuron model has different impact (amplifies current injected) than gain in synapse model (amplifies weights)
         self.params['K'] = 0.
-        self.params['fmax'] = 35. #100.  #70
+        self.params['fmax'] = 40. #100.  #70
         self.params['rp_fmax'] = 35.   # 
         self.params['rpe'] = 0.01
+        self.params['positive_prior'] = .01
 
         ########### NEURON PARAMETERS
         self.params['Vth'] = -50. 
         self.params['Cm'] = 250.
         self.params['Vreset'] = -80.
         self.params['gL'] = 16.6667
-        self.params['temperature'] = 3.
+        self.params['temperature'] = 20.
 
         self.params['Cm_std'] = 5. #0.01  #25.
         self.params['Vth_std'] = 1. #0.01   #2.
@@ -158,8 +159,12 @@ class global_parameters(ParameterContainer.ParameterContainer):
         
         self.params['str_to_output_exc_w'] = 4.             ### D2
         self.params['str_to_output_inh_w'] = -1.   #-1.     ### D1
-        self.params['str_to_output_exc_delay'] = 1. 
-        self.params['str_to_output_inh_delay'] = 1.
+        self.params['str_to_output_exc_delay'] = 2. 
+        self.params['str_to_output_inh_delay'] = 2.
+        self.params['std_str_to_output_exc_w'] = .2             ### D2
+        self.params['std_str_to_output_inh_w'] = .1   #-1.     ### D1
+        self.params['std_str_to_output_exc_delay'] = .2 
+        self.params['std_str_to_output_inh_delay'] = .2
 
         # ========================
         # RP and REWARD 
@@ -186,19 +191,40 @@ class global_parameters(ParameterContainer.ParameterContainer):
         # ========================
         #Connections Actions and States to RP
         # parameters for the standard bcpnn connections
-        self.params['p_i'] = np.random.random()/33.
-        self.params['p_j'] = np.random.random()/33.
-       # self.params['p_ij']= self.params['p_i'] * self.params['p_j']
-        self.params['p_ij']= (np.random.random()/33.)*(np.random.random()/33.)
+        self.params['p_i'] =   .1   # np.random.random()/33.
+        self.params['p_j'] =   .1   # np.random.random()/33.
+        self.params['p_ij']=   .01   # self.params['p_i'] * self.params['p_j'] + np.random.random()/30.
+       # self.params['p_ij']= (np.random.random()/20.)*(np.random.random()/20.)
        # print 'InitialP Pi', self.params['p_i'], 'pj',self.params['p_j'] , 'Pij', self.params['p_ij'] 
        # self.params['p_i'] = self.params['t_selection'] / ( self.params['n_states']*self.params['t_iteration']  ) 
        # self.params['p_j'] = ( self.params['t_reward'] + self.params['t_efference'] ) / ( self.params['n_actions']*self.params['t_iteration']  )
        # self.params['p_ij']= self.params['p_i'] * self.params['p_j']
 
-        self.params['p_i_std']= self.params['p_i']/10.
-        self.params['p_j_std']= self.params['p_j']/10.
-        self.params['p_ij_std']= self.params['p_ij']/10.
+        self.params['p_i_d1']  =   self.params['t_selection'] / ( self.params['n_states']*self.params['t_iteration'] ) 
+        self.params['p_j_d1']  = ( self.params['t_reward'] + self.params['t_efference'] ) / ( self.params['n_actions']*self.params['t_iteration'] )
+        self.params['p_ij_d1'] =   self.params['p_i_d1'] * self.params['p_j_d1'] + self.params['positive_prior'] 
+        self.params['std_p_i_d1']  = self.params['p_i_d1'] * 10./100 
+        self.params['std_p_j_d1']  = self.params['p_j_d1'] * 10./100  
+        self.params['std_p_ij_d1'] = self.params['p_ij_d1'] * 10./100 
+
+
+        self.params['p_i_d2']  =   self.params['t_selection'] / ( self.params['n_states']*self.params['t_iteration'] ) 
+        self.params['p_j_d2']  = ( self.params['t_reward'] + self.params['t_efference'] ) / ( self.params['n_actions']*self.params['t_iteration'] )
+        self.params['p_ij_d2'] =   self.params['p_i_d2'] * self.params['p_j_d2'] + self.params['positive_prior'] 
+        self.params['std_p_i_d2']  = self.params['p_i_d2'] * 10./100 
+        self.params['std_p_j_d2']  = self.params['p_j_d2'] * 10./100  
+        self.params['std_p_ij_d2'] = self.params['p_ij_d2'] * 10./100 
+
         
+        self.params['delay_d1'] = 2.
+        self.params['delay_d2'] = 2.
+        self.params['std_delay_d1'] = .2
+        self.params['std_delay_d2'] = .2
+        
+
+
+
+
         # ========================
         #  
         # ========================
@@ -206,6 +232,18 @@ class global_parameters(ParameterContainer.ParameterContainer):
         self.params['p_ir'] = 0.01
         self.params['p_jr'] = 0.01
         self.params['p_ijr']= 0.0001
+
+        #self.params['p_i_rp']  =   self.params['t_selection'] / ( self.params['n_states']*self.params['t_iteration'] ) 
+        #self.params['p_j_rp']  =   self.params['t_efference'] / ( self.params['n_actions']*self.params['n_states']*self.params['t_iteration'] )
+        self.params['p_i_rp']  =  self.params['p_ir'] 
+        self.params['p_j_rp']  =  self.params['p_jr'] 
+        self.params['p_ij_rp'] =   self.params['p_i_rp'] * self.params['p_j_rp'] #+ self.params['positive_prior']
+        self.params['std_p_i_rp']  = self.params['p_i_rp'] * 10./100 
+        self.params['std_p_j_rp']  = self.params['p_j_rp'] * 10./100  
+        self.params['std_p_ij_rp'] = self.params['p_ij_rp'] * 10./100 
+
+        self.params['delay_rp'] = 2.
+        self.params['std_delay_rp'] = .2
 
         self.params['actions_rp'] = 'bcpnn_dopamine_synapse'
         self.params['param_actions_rp'] = {'p_i': self.params['p_ir'], 'p_j': self.params['p_jr'], 'p_ij': self.params['p_ijr'], 
@@ -228,13 +266,17 @@ class global_parameters(ParameterContainer.ParameterContainer):
         self.params['params_lateral_synapse_d1'] = {}
         self.params['params_lateral_synapse_d2'] = {}
 
-        self.params['inhib_lateral_weights_d1'] = -4. #-4.
-        self.params['inhib_lateral_weights_d2'] = -4. #-4.
-        self.params['inhib_lateral_weights_d2_d1'] = -1. #-4.
+        #self.params['inhib_lateral_weights_d2_d1'] = -1. #-4.
+        self.params['inhib_lateral_weights_d1'] = -1. #-4.
+        self.params['inhib_lateral_weights_d2'] = -1. #-4.
         self.params['inhib_lateral_delay_d1'] = 1.
         self.params['inhib_lateral_delay_d2'] = 1.
-        self.params['ratio_lat_inh_d1_d1'] = 3.   # ratio of D1 MSNs belonging to the other actions inhibited by one D1 MSN from a specific action  
-        self.params['ratio_lat_inh_d2_d2'] = 3.   # ratio of D1 MSNs belonging to the other actions inhibited by one D1 MSN from a specific action
+        self.params['std_inhib_lateral_weights_d1'] = .01 #-4.
+        self.params['std_inhib_lateral_weights_d2'] = .01 #-4.
+        self.params['std_inhib_lateral_delay_d1'] = .1
+        self.params['std_inhib_lateral_delay_d2'] = .1
+        self.params['ratio_lat_inh_d1_d1'] = 2.   # ratio of D1 MSNs belonging to the other actions inhibited by one D1 MSN from a specific action  
+        self.params['ratio_lat_inh_d2_d2'] = 2.   # ratio of D1 MSNs belonging to the other actions inhibited by one D1 MSN from a specific action
         # during learning gain == 0. K = 1.0 : --> 'offline' learning
         # after learning: gain == 1. K = .0
 
@@ -260,14 +302,26 @@ class global_parameters(ParameterContainer.ParameterContainer):
         #Connections REW to RP, STRD1 and STRD2
         self.params['weight_rew_strD1'] = 4.
         self.params['weight_rew_strD2'] = 4.
-        self.params['delay_rew_strD1'] = 1.
-        self.params['delay_rew_strD2'] = 1.
+        self.params['delay_rew_strD1'] = 2.
+        self.params['delay_rew_strD2'] = 2.
+        self.params['std_weight_rew_strD1'] = .2
+        self.params['std_weight_rew_strD2'] = .2
+        self.params['std_delay_rew_strD1'] = .2
+        self.params['std_delay_rew_strD2'] = .2
 
         self.params['weight_rew_rp'] = 4.
-        self.params['delay_rew_rp'] = 1.
+        self.params['delay_rew_rp'] = 2.
+        self.params['std_weight_rew_rp'] = .2
+        self.params['std_delay_rew_rp'] = .2
 
         self.params['w_rew_vtdopa'] = 1.
-        self.params['delay_rew_vtdopa'] = 1.
+        self.params['w_rew_vtdopa'] = 1.
+        self.params['delay_rew_vtdopa'] = 2.
+        self.params['delay_rew_vtdopa'] = 2.
+        self.params['std_w_rew_vtdopa'] = .1
+        self.params['std_w_rew_vtdopa'] = .1
+        self.params['std_delay_rew_vtdopa'] = .2
+        self.params['std_delay_rew_vtdopa'] = .2
 
 
         # ========================
@@ -294,9 +348,9 @@ class global_parameters(ParameterContainer.ParameterContainer):
 
         self.params['num_neuron_poisson_efference'] = 1
         self.params['num_neuron_poisson_input_BG'] =1 
-        self.params['active_full_efference_rate'] = 500.  #1800. #2000.#600.  #3000.
+        self.params['active_full_efference_rate'] = 700.
         self.params['inactive_efference_rate'] = 1.
-        self.params['active_poisson_input_rate'] = 1900.  #1750.   #1900.  #2500.
+        self.params['active_poisson_input_rate'] = 1950.  #1750.   #1900.  #2500.
         self.params['inactive_poisson_input_rate'] = 1500. #1400.
         self.params['supervisor_off'] = 0.
 
@@ -305,8 +359,8 @@ class global_parameters(ParameterContainer.ParameterContainer):
         self.params['inactive_poisson_rew_rate'] = 2300.  #2300.
         
         #Initialisation####################
-        self.params['initial_poisson_input_rate'] = 1500.  #2500.
-        self.params['initial_noise_striatum_rate'] = 2000.  #2500.
+        self.params['initial_poisson_input_rate'] = 1700.  #2500.
+        self.params['initial_noise_striatum_rate'] = 2500.  #2500.
 
         self.params['param_poisson_pop_input_BG'] = {}
         self.params['param_poisson_efference'] = {}
@@ -317,14 +371,22 @@ class global_parameters(ParameterContainer.ParameterContainer):
         self.params['delay_poisson_rew'] = 1.
         self.params['param_poisson_rew'] = {}# to adapt parms to aif_cond_alpha neuron model
 
-        self.params['weight_efference_strd1_exc'] = 1.      ## 4.
-        self.params['weight_efference_strd1_inh'] = -1.     ## -2.
-        self.params['weight_efference_strd2_exc'] = 1.
-        self.params['weight_efference_strd2_inh'] = -1.
-        self.params['delay_efference_strd1_exc'] = 1.
-        self.params['delay_efference_strd1_inh'] = 1.
-        self.params['delay_efference_strd2_exc'] = 1.
-        self.params['delay_efference_strd2_inh'] = 1.
+        self.params['weight_efference_strd1_exc'] = 4.      ## 4.
+        self.params['weight_efference_strd1_inh'] = -3.     ## -2.
+        self.params['weight_efference_strd2_exc'] = 4.
+        self.params['weight_efference_strd2_inh'] = -3.
+        self.params['std_weight_efference_strd1_exc'] = .1     ## 4.
+        self.params['std_weight_efference_strd1_inh'] = .1    ## -2.
+        self.params['std_weight_efference_strd2_exc'] = .1
+        self.params['std_weight_efference_strd2_inh'] = .1
+        self.params['delay_efference_strd1_exc'] = 2.
+        self.params['delay_efference_strd1_inh'] = 2.
+        self.params['delay_efference_strd2_exc'] = 2.
+        self.params['delay_efference_strd2_inh'] = 2.
+        self.params['std_delay_efference_strd1_exc'] = .2
+        self.params['std_delay_efference_strd1_inh'] = .2
+        self.params['std_delay_efference_strd2_exc'] = .2
+        self.params['std_delay_efference_strd2_inh'] = .2
 
         self.params['weight_poisson_input'] = 4.
         self.params['delay_poisson_input'] = 1.
@@ -332,10 +394,14 @@ class global_parameters(ParameterContainer.ParameterContainer):
         self.params['num_neuron_states'] = 30
         self.params['param_states_pop'] = {} 
 
-        self.params['weight_states_rp'] = 3.  #3.
-        self.params['delay_states_rp'] =   self.params['t_efference']
-        self.params['weight_efference_rp'] = 4.
+        self.params['weight_states_rp']    = 5.  #3.
+        self.params['weight_efference_rp'] = 5.
+        self.params['delay_states_rp']    =  self.params['t_efference']
         self.params['delay_efference_rp'] =  self.params['t_efference']
+        self.params['std_weight_states_rp']    = .5  
+        self.params['std_weight_efference_rp'] = .5
+        self.params['std_delay_states_rp']    = 5.
+        self.params['std_delay_efference_rp'] = 5.
        # self.params['weight_actions_rp'] = 5.
        # self.params['delay_actions_rp'] =  self.params['t_efference']
 
@@ -433,7 +499,7 @@ class global_parameters(ParameterContainer.ParameterContainer):
         self.params['tau_ib'] = 5.
         self.params['tau_jb'] = 6.
         self.params['tau_eb'] = 50.
-        self.params['tau_pb'] = 100000.
+        self.params['tau_pb'] = 20000.
 
 
         self.params['model_brainstem_neuron']= 'iaf_cond_alpha_bias'
@@ -443,7 +509,7 @@ class global_parameters(ParameterContainer.ParameterContainer):
                     'epsilon': self.params['epsilon'], 't_ref': 2.0, 'gain': self.params['gainb_neuron']}
 
         self.params['synapse_states_brainstem'] = 'bcpnn_synapse'
-        self.params['weight_actions_brainstem'] = 5.
+        self.params['weight_actions_brainstem'] = -7.
         self.params['delay_actions_brainstem'] = 1.
         self.params['params_synapse_states_brainstem'] = {'p_i':self.params['p_i'], 'p_j': self.params['p_j'], 'p_ij': self.params['p_ij'], 
                 'gain': self.params['gainb'], 'K': self.params['Kb'], 'fmax': self.params['fmax'],
@@ -461,9 +527,9 @@ class global_parameters(ParameterContainer.ParameterContainer):
         self.params['noise_weight_actions_exc']= 1.5
         self.params['noise_weight_actions_inh']= 1.5
         self.params['noise_weight_str_exc']= 1.5
-        self.params['noise_weight_rp_exc']= 3.
+        self.params['noise_weight_rp_exc']= 2.
         self.params['noise_weight_rp_inh']= 1.5
-        self.params['noise_weight_bs_exc']= 3.
+        self.params['noise_weight_bs_exc']= 1.
         self.params['noise_weight_bs_inh']= 1.
         self.params['noise_delay_d1_exc']= 1. 
         self.params['noise_delay_d1_inh']= 1. 
@@ -485,7 +551,7 @@ class global_parameters(ParameterContainer.ParameterContainer):
         self.params['noise_rate_str_exc']=1000. #1000. 
         self.params['noise_rate_rp_exc']=2500.  
         self.params['noise_rate_rp_inh']=1000.  
-        self.params['noise_rate_bs_exc']=3000.  
+        self.params['noise_rate_bs_exc']=4000.  
         self.params['noise_rate_bs_inh']=1000.  
 
 